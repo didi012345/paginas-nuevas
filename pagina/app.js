880a67c47564c3ca9bcfdc2edd24b77dd3d0888e
didi012345 <112119050+didi012345@users.noreply.github.com>
2022-10-29 20:23:43 -0500

    Add files via upload

---
+++ pagina/app.js
@@ -0,0 +1,11 @@
+// Typing animation effect
+var typed = new Typed(".effect",{
+    strings:[
+        "YouTuber","Developer",
+        "Blogger", "Designer",
+        "Freelancer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
